fb6a26da9fea7bfe6a28b0f0097816967130a6cb
Wonwoo Jin <jamesjin06@gmail.com>
2022-04-04 21:47:10 +0900

    7th commit

--- resource/java_script/index.js
+++ resource/java_script/index.js
@@ -5,9 +5,12 @@ setpage();
 function setpage() {
     if(name == "") {
         document.write('<p><a href="login.html">로그인<a/></p>');
+        // https://cravatar.eu/helmavatar/_cozy__
+        // <img src="https://cravatar.eu/helmavatar/_cozy__" alt="My Image">
     }
     else {
         document.write('<p>' + name + "님 환영합니다!<br></p>");
+        document.write('<p><img src="https://cravatar.eu/helmavatar/' + name + "\" alt=\"My Image\" width=\"100\" height=\"100\">");
     }
 }
 
