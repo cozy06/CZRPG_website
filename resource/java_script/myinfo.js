7a8b1e07a1d026a691dd19b61901fd1f0d716a9f
Wonwoo Jin <jamesjin06@gmail.com>
2022-04-07 01:08:03 +0900

    9th commit

--- resource/java_script/myinfo.js
+++ resource/java_script/myinfo.js
@@ -3,12 +3,7 @@ var name = getParam("name")
 setpage();
 
 function setpage() {
-    if(name == "") {
-        document.write('<p><a href="login.html">로그인<a/></p>');
-    }
-    else {
-        document.write('<p><a href="myinfo.html' + "?name=" + name + "\">" + name + "님</a>" + " 환영합니다!<br></p>");
-    }
+    document.write('<p><img src="https://cravatar.eu/helmavatar/' + name + "/200.png\" alt=\"minecraft skin\" width=\"200\" height=\"200\">");
 }
 
 function getParam(sname) {
