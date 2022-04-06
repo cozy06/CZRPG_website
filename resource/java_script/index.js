f31baa38639cf7b7d997711762d22907ebdca148
Wonwoo Jin <jamesjin06@gmail.com>
2022-04-06 23:48:02 +0900

    8th commit

--- resource/java_script/index.js
+++ resource/java_script/index.js
@@ -7,7 +7,7 @@ function setpage() {
         document.write('<p><a href="login.html">로그인<a/></p>');
     }
     else {
-        document.write('<p>' + name + "님 환영합니다!<br></p>");
+        document.write('<p><a href="myinfo.html">' + name + "님</a>" + " 환영합니다!<br></p>");
         document.write('<p><img src="https://cravatar.eu/helmavatar/' + name + "/200.png\" alt=\"minecraft skin\" width=\"200\" height=\"200\">");
     }
 }
